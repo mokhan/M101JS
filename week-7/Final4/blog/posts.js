191a4f4daa4c54357db20ee05b5bdb2a7ca5eada
mo khan <mo@mokhan.ca>
2015-02-21 21:19:03 -0700

    solve final 4.

--- week-7/Final4/blog/posts.js
+++ week-7/Final4/blog/posts.js
@@ -126,7 +126,15 @@ function PostsDAO(db) {
         // is zero indexed).
 
         // TODO: Final exam question - Increment the number of likes
-        callback(Error("incrementLikes NYI"), null);
+        //callback(Error("incrementLikes NYI"), null);
+        var selector = {};
+        selector['comments.' + comment_ordinal + '.num_likes'] = 1
+        posts.update({'permalink': permalink}, {'$inc': selector}, function(err, numModified) {
+            "use strict";
+            if (err) return callback(err, null);
+            callback(err, numModified);
+        });
+
     }
 }
 
